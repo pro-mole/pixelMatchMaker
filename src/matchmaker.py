7abaa0c8c064349bcf963846a0c5b61d59460fcb
Professional Mole <professional.mole@yahoo.com.br>
2013-05-09 17:03:10 -0300

    Tester

--- src/matchmaker.py
+++ src/matchmaker.py
@@ -87,62 +87,65 @@ def pickrandom(C):
     
     return random.choice(_C)
 
-dataset = sys.argv[1]
-#print dataset
+#Modularizing our script, for intense testing
+# Input parameters are the input files
+def solve(pairs, query):
+    #Compatibility Set
+    # The professions, laid out in compatible pairs
+    compatibility = []
 
-pairs = open(dataset+"/pairs","r")
-query = open(dataset+"/query","r")
+    #Query Set
+    # The set of vertices of the query graph
+    query_set = []
 
-#Compatibility Set
-# The professions, laid out in compatible pairs
-compatibility = []
+    #Query Graph
+    # The set of edges of the query graph; based on the profession graph applied over the query set
+    query_graph = []
 
-#Query Set
-# The set of vertices of the query graph
-query_set = []
+    #Solution
+    # Ta-da!
+    solution=[]
 
-#Query Graph
-# The set of edges of the query graph; based on the profession graph applied over the query set
-query_graph = []
+    #Read pairs, generate compatibility set
+    for pair in pairs.readlines():
+        T = tuple(str(P) for P in re.findall("[A-Za-z ]+",pair))
+        _T = T[1], T[0]
+        if compatibility.count(T) + compatibility.count(_T) == 0:
+            compatibility.append(T)
+            
+    #print compatibility
 
-#Solution
-# Ta-da!
-solution=[]
+    #Read query, generate query set
+    for q in query.readlines():
+        query_set.append(q.strip())
 
-#Read pairs, generate compatibility set
-for pair in pairs.readlines():
-    T = tuple(str(P) for P in re.findall("[A-Za-z ]+",pair))
-    _T = T[1], T[0]
-    if compatibility.count(T) + compatibility.count(_T) == 0:
-        compatibility.append(T)
-        
-#print compatibility
+    query_set = list(enumerate(query_set))
 
-#Read query, generate query set
-for q in query.readlines():
-    query_set.append(q.strip())
+    #print query_set
 
-query_set = list(enumerate(query_set))
+    #Apply compatibility set on query, generate the query graph
+    for P in compatibility:
+        v1,v2 = P
+        for A in [(i,Q) for i,Q in query_set if Q == v1]:
+            for B in [(i,Q) for i,Q in query_set if Q == v2]:
+                if (A != B) and edge_count(query_graph,A,B) == 0:
+                    query_graph.append((A,B))
 
-#print query_set
+    #print query_graph
+    #print cardinality(query_set, query_graph)
 
-#Apply compatibility set on query, generate the query graph
-for P in compatibility:
-    v1,v2 = P
-    for A in [(i,Q) for i,Q in query_set if Q == v1]:
-        for B in [(i,Q) for i,Q in query_set if Q == v2]:
-            if (A != B) and edge_count(query_graph,A,B) == 0:
-                query_graph.append((A,B))
+    #print "Start Solution:"
+    solution = solve_heurist1(query_set, query_graph)
+    #for v in solution[0]:
+    #    print "{0}: {1}".format(v[0], v[1])
+    #print
+    #for e in solution[1]:
+    #    print "{0} <-> {1}".format(e[0][0],e[1][0])
+    #print
+    #print "Optimality: {0}".format(optimality(solution[0], solution[1]))
+    
+    return(solution, optimality(solution[0], solution[1]))
 
-#print query_graph
-#print cardinality(query_set, query_graph)
-
-print "Start Solution:"
-solution = solve_heurist1(query_set, query_graph)
-for v in solution[0]:
-    print "{0}: {1}".format(v[0], v[1])
-print
-for e in solution[1]:
-    print "{0} <-> {1}".format(e[0][0],e[1][0])
-print
-print "Optimality: {0}".format(optimality(solution[0], solution[1]))
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        solve(open(sys.argv[1],"r"),open(sys.argv[2],"r"))
